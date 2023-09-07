8fd00e6bb3deddb7da518d8315ed1921a113b772
farhanacsebd <farhana.cse.bd@gmail.com>
2023-09-07 15:04:34 +0600

    getone,delete or update

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -2,6 +2,7 @@ const {v4: uuidv4} = require("uuid");
 const User = require("../models/user.model");
 
 
+// get all users
 const getAllUsers = async (req,res) =>{
 
    try{
@@ -13,12 +14,21 @@ const getAllUsers = async (req,res) =>{
    }
 };
 
-const getOneUser = (req,res) =>{
-    res.status(200).json({
-        message: "get one user",
-    })
+
+// get only one user
+const getOneUser = async(req,res) =>{
+
+    try{
+        const user = await User.findOne({id: req.params.id});
+        res.status(200).json(user)
+    }
+    catch{
+        res.status(500).send(error.message)
+    }
 };
 
+
+//create uses
 const createUser = async (req,res) =>{
 
     try{
@@ -35,16 +45,35 @@ const createUser = async (req,res) =>{
     }
 };
 
-const updatedUser = (req,res) =>{
-    res.status(200).json({
-        message: "user is updated",
-    })
+
+//update use
+const updatedUser = async(req,res) =>{
+
+    try{
+        const user = await User.findOne({id: req.params.id});
+    user.name = req.body.name;
+    user.age = Number(req.body.age);
+
+    await user.save();
+    
+    res.status(200).json(user)
+    }
+    catch{
+        res.status(500).send(error.message)
+    }
 };
 
-const deleteUser = (req,res) =>{
-    res.status(200).json({
-        message: "user is deleted",
-    })
+
+//delete user
+const deleteUser = async(req,res) =>{
+
+    try{
+        await User.deleteOne({id: req.params.id});
+        res.status(200).json({message: "user is deleted"})
+    }
+    catch{
+        res.status(500).send(error.message)
+    }
 };
 
 
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,11 +1,14 @@
 const express = require("express");
-const {getAllUsers, createUser} = require("../controllers/user.controllers");
+const {getAllUsers, createUser, getOneUser, deleteUser, updatedUser} = require("../controllers/user.controllers");
 
 const router = express.Router();
 
 
 router.get("/", getAllUsers);
+router.get("/:id", getOneUser);
 router.post("/", createUser);
+router.patch("/:id", updatedUser);
+router.delete("/:id", deleteUser);
 
 
 module.exports = router;
